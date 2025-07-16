ef27ab4203ef88d0e0846109eaec44967841c145
DmrAutoConcept <dmrautoconcept57@gmail.com>
2025-07-16 13:44:40 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bienvenue sur DMR Auto Concept</h1>
-      <p>Assistant intelligent pour trouver votre véhicule idéal en Suisse.</p>
-    </div>
-  );
-}
