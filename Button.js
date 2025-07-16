81ffa17e78f2dd0e88a22f08316e9de7ceaaeee9
DmrAutoConcept <dmrautoconcept57@gmail.com>
2025-07-16 13:49:24 +0200

    Delete Button.js

--- Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({ children }) {
-  return (
-    <button style={{ padding: '10px 20px', backgroundColor: '#000', color: '#fff', borderRadius: '5px' }}>
-      {children}
-    </button>
-  );
-}
